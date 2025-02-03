089758dc268e2090573bbd6d1a15bce2c48fbbb5
krmkarakoc <keremkarakoc@ibaysoft.com.tr>
2025-02-03 13:30:32 +0300

    initial

---
+++ src/main/java/com/kaya/erp/kayaerp/KayaerpApplication.java
@@ -0,0 +1,13 @@
+package com.kaya.erp.kayaerp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KayaerpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KayaerpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kaya/erp/kayaerp/KayaerpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaya.erp.kayaerp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KayaerpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
