2dc8b5b75072fe123c2b7a3011877e86a4cb61da
krmkarakoc <keremkarakoc@ibaysoft.com.tr>
2025-03-28 13:24:21 +0300

    fotograf v1

--- src/main/java/com/kaya/erp/kayaerp/util/AppUtil.java
+++ src/main/java/com/kaya/erp/kayaerp/util/AppUtil.java
@@ -7,18 +7,22 @@ import java.time.format.DateTimeFormatter;
 import java.util.Date;
 import java.util.List;
 
+import com.kaya.erp.kayaerp.entity.dto.DtoImageInfo;
+import com.kaya.erp.kayaerp.entity.model.Fotograf;
+
 public final class AppUtil {
 
     private static final DateTimeFormatter DEFAULT_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 
     private static final String DEFAULT_DATE_PATTERN = "yyyy-MM-dd HH:mm:ss";
+    private static final String DEFAULT_DATE_PATTERN_FOR_PHOTOS = "yyyy_MM_dd&HH_mm_ss";
     
     // private constructor to prevent instantiation
     private AppUtil() {
         throw new UnsupportedOperationException("Utility class");
     }
 
-    public static String formatDateToLocalDateTime(LocalDateTime dateTime) {
+    public static String formatDateFromLocalDateTimeToString(LocalDateTime dateTime) {
         if (dateTime == null) return "";
         return DEFAULT_FORMATTER.format(dateTime);
     }
@@ -35,6 +39,19 @@ public final class AppUtil {
 
         try {
             SimpleDateFormat sdf = new SimpleDateFormat(DEFAULT_DATE_PATTERN);
+            return sdf.parse(dateStr);
+        } catch (ParseException e) {
+            throw new IllegalArgumentException("Geçersiz tarih formatı: " + dateStr);
+        } 
+    }
+    
+    public static Date parseDateFromStringToDateForPhotos(String dateStr) {
+        if (dateStr == null || dateStr.trim().isEmpty()) {
+            return null;
+        }
+
+        try {
+            SimpleDateFormat sdf = new SimpleDateFormat(DEFAULT_DATE_PATTERN_FOR_PHOTOS);
             return sdf.parse(dateStr);
         } catch (ParseException e) {
             throw new IllegalArgumentException("Geçersiz tarih formatı: " + dateStr);
@@ -57,6 +74,18 @@ public final class AppUtil {
     
         public static <E> boolean isObjectNull(E object) {
             return object == null;
+        }
+        
+        
+        public static Fotograf mapToFotograf(DtoImageInfo dto, String type, String sasi, String pname,Date ptarih) {
+            Fotograf fotograf = new Fotograf();
+            fotograf.setType(type);       // Bu alanları sen dışardan vereceksin
+            fotograf.setSasi(sasi);
+            fotograf.setPname(pname);
+            fotograf.setPath(dto.getFilePath());
+            fotograf.setPtarih(ptarih);
+          
+            return fotograf;
         }
     }
 
